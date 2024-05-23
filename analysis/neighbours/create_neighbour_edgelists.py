e981a079068d0a9402ba785002b882f4edb3fcdc
Oliver Lloyd <fu19841@bristol.ac.uk>
2024-05-23 13:38:30 +0100

    merge

--- analysis/neighbours/create_neighbour_edgelists.py
+++ analysis/neighbours/create_neighbour_edgelists.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from torch import tensor, save
+from itertools import combinations
 
 
 def add_reverse_edges(df):
@@ -15,61 +16,65 @@ if __name__ == '__main__':
     selfloops_ids = pd.read_csv('../../../kge/data/selfloops/entity_ids.del', sep='\t', header=None)
     name_to_id = {row[1]: row[0] for _, row in selfloops_ids.iterrows()}
 
+
+    data = {
+        'RDK': {},
+        'MAP4': {},
+        'morgan': {},
+    }
+
     # Load RDK similarity data
     RDK_raw = pd.read_csv('../../../Chapter4/data/processed/drug_fingerprint_similarity.csv')
-    RDK_raw = add_reverse_edges(RDK_raw)  # we DO need to add reverse edges here because similarity results go both ways which isnt represented in the csv. Filtered to closest ten anyway
+    data['RDK']['raw'] = add_reverse_edges(RDK_raw)  # we DO need to add reverse edges to all these dfs, despite what I keep believing and wasting time on
+    data['RDK']['target_col'] = 'fingerprint_similarity'
 
     # Load MAP4 similarity data
     map4_raw = pd.read_csv('MAP4/MAP4_minhash.csv')
-    map4_raw = add_reverse_edges(map4_raw)
+    data['MAP4']['raw'] = add_reverse_edges(map4_raw)
+    data['MAP4']['target_col'] = 'minhash_similarity'
 
     # Load Morgan similarity data
-    morgan_raw = pd.read_csv('morgan_tanimoto/morgan_tanimoto.csv')
-    morgan_raw = add_reverse_edges(morgan_raw)
+    morgan_raw = pd.read_csv('morgan/morgan_tanimoto.csv')
+    data['morgan']['raw'] = add_reverse_edges(morgan_raw)
+    data['morgan']['target_col'] = 'morgan_tanimoto'
 
+    neighbourless = []
+    
     # Filter by n closest neighbours for each drug
     for n in [5, 10, 20]:
+        nearest_n_str = f'nearest_{n}'
+        for neighbour_method in data.keys():
 
-        # Using tanimoto coefficient of RDK fingerprints
-        RDK = []
-        for drug, subdf in RDK_raw.groupby('drug1'):
-            subdf.sort_values('fingerprint_similarity', ascending=False, inplace=True)
-            RDK.append(subdf.iloc[:n])
-        RDK = pd.concat(RDK)
 
-        RDK['id1'] = [name_to_id[name] for name in RDK.drug1.values]
-        RDK['id2'] = [name_to_id[name] for name in RDK.drug2.values]
-        RDK_out = tensor(RDK[['id1', 'id2']].to_numpy())
+            target_col = data[neighbour_method]['target_col']
+            out = []
+            for drug, subdf in data[neighbour_method]['raw'].groupby('drug1'):
+                subdf.query(f'{target_col} > 0', inplace=True)
+                n_neighbours = len(subdf)
+                if n_neighbours < n:
+                    neighbourless.append([neighbour_method, n, drug, n_neighbours])
+                if n_neighbours > 1:    
+                    subdf.sort_values(target_col, ascending=False, inplace=True)
+                    out.append(subdf.iloc[:n])
+            out = pd.concat(out)
+
+            out['id1'] = [name_to_id[name] for name in out.drug1.values]
+            out['id2'] = [name_to_id[name] for name in out.drug2.values]
+            out_tensor = tensor(out[['id1', 'id2']].to_numpy())
+            data[neighbour_method][nearest_n_str] = out_tensor
+
+        # Assert equivalence of neighbourhood graphs
+        for key, key2 in combinations(data.keys(), 2):
+            pass#assert data[key][nearest_n_str].shape == data[key2][nearest_n_str].shape
         
+        # Save
+        for key in data.keys():
+            save(data[key][nearest_n_str], f'{key}/{key}_{nearest_n_str}.pt')
 
-        # Using MAP4 minhash
-        map4 = []
-        for drug, subdf in map4_raw.groupby('drug1'):
-            subdf.sort_values('minhash_similarity', ascending=False, inplace=True)
-            map4.append(subdf.iloc[:n])
-        map4 = pd.concat(map4)
-
-        map4['id1'] = [name_to_id[name] for name in map4.drug1.values]
-        map4['id2'] = [name_to_id[name] for name in map4.drug2.values]
-        map4_out = tensor(map4[['id1', 'id2']].to_numpy())
-
-        # Using Morgan tanimoto
-        morgan = []
-        for drug, subdf in morgan_raw.groupby('drug1'):
-            subdf.sort_values('morgan_tanimoto', ascending=False, inplace=True)
-            morgan.append(subdf.iloc[:n])
-        morgan = pd.concat(morgan)
-
-        morgan['id1'] = [name_to_id[name] for name in morgan.drug1.values]
-        morgan['id2'] = [name_to_id[name] for name in morgan.drug2.values]
-        morgan_out = tensor(morgan[['id1', 'id2']].to_numpy())
-
-        # Assert equivalence then save
-        assert map4_out.shape == RDK_out.shape
-        assert morgan_out.shape == RDK_out.shape
-        save(RDK_out, f'RDK_tanimoto/RDK_nearest{n}.pt')
-        save(map4_out, f'MAP4/MAP4_nearest{n}.pt')
-        save(morgan_out, f'morgan_tanimoto/morgan_nearest{n}.pt')
+    # Do stuff if found some neighbourless drugs
+    if len(neighbourless):
+        neighbourless = pd.DataFrame(neighbourless, columns=['similarity_method', 'nearest_N', 'drug', 'actual_N'])
+        neighbourless.to_csv('neighbourless.csv', index=False)
 
     # Using drug-gene bipartite projection edges (no nearest-n performed)
     gene_bipartite = pd.read_csv('../../../Chapter4/data/processed/drug_projection_edges.csv')
