550a57ea7f0fa934624696577b4683a564acdf21
fu19841 <fu19841@bp1-login03.data.bp.acrc.priv>
2024-05-29 09:55:16 +0100

    Analysis part 2 results

---
+++ analysis/2_minimising_psi/experiments/downstream_analyse.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo Creating aggregates;
+python ../../../scripts/get_centroid_aggregates.py
+
+echo Getting cosines;
+python ../../../scripts/get_cosine_with_actual.py;
+
+echo Getting per-epoch runtime;
+python ../../../scripts/get_per-epoch_runtime.py
+
+mkdir -p holdout_scores
+echo Getting polySE scores with aggregations;
+python ../../../scripts/polySE_with_aggregates.py
+
+#echo Getting POSTHOC polySE scores with aggregations;
+#python ../../../scripts/polySE_with_aggregates.py --posthoc_removal
+
+echo Getting polySE scores with actual embeds;
+sh ../../../scripts/polySE_with_true_embeds.sh;
